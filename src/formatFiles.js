9ec3d60e98d0f26bd0ba8e686ac758d978a03bad
aleclarson <alec.stanford.larson@gmail.com>
2018-12-13 13:12:27 -0500

    feat: use "prettier-tslint" and "prettier-eslint" when they exist
    When the scm root has "./node_modules/prettier-tslint" and/or
    "./node_modules/prettier-eslint", we use them on applicable modules.

--- src/formatFiles.js
+++ src/formatFiles.js
@@ -1,5 +1,5 @@
-import { readFileSync, writeFileSync } from 'fs';
-import { resolveConfig, format } from 'prettier';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
+import { resolveConfig, format as prettierFormat } from 'prettier';
 import { join } from 'path';
 
 export default (
@@ -7,17 +7,30 @@ export default (
   files,
   { config, onWriteFile, onExamineFile } = {}
 ) => {
+  // Use local "prettier-tslint" if possible.
+  const tslintFormat = loadFormat(rootDirectory, 'prettier-tslint', 'format');
+  // Use local "prettier-eslint" if possible.
+  const eslintFormat = loadFormat(rootDirectory, 'prettier-eslint');
+
   for (const relative of files) {
     onExamineFile && onExamineFile(relative);
     const file = join(rootDirectory, relative);
-    const options = resolveConfig.sync(file, { config, editorconfig: true });
     const input = readFileSync(file, 'utf8');
-    const output = format(
-      input,
-      Object.assign({}, options, {
-        filepath: file,
-      })
-    );
+
+    const format =
+      (/\.(mjs|jsx?)$/.test(file) && (eslintFormat || tslintFormat)) ||
+      (/\.tsx?$/.test(file) && (tslintFormat || eslintFormat)) ||
+      prettierFormat;
+
+    const options = resolveConfig.sync(file, { config, editorconfig: true });
+    const output =
+      format === prettierFormat
+        ? format(input, ((options.filepath = file), options))
+        : format({
+            text: input,
+            filePath: file,
+            prettierOptions: options,
+          });
 
     if (output !== input) {
       writeFileSync(file, output);
@@ -25,3 +38,11 @@ export default (
     }
   }
 };
+
+function loadFormat(rootDirectory, formatName, exportKey) {
+  const formatPath = join(rootDirectory, 'node_modules', formatName);
+  if (existsSync(formatPath)) {
+    const exports = require(formatPath);
+    return exportKey ? exports[exportKey] : exports;
+  }
+}
