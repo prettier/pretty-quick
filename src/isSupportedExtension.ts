37f3e525360694624312a5e4b58c9444ee6c69ee
Conrad Hughes <90639+ConradHughes@users.noreply.github.com>
2025-03-04 18:09:59 +0000

    style: get rid of existsSync

--- src/isSupportedExtension.ts
+++ src/isSupportedExtension.ts
@@ -1,6 +1,6 @@
 /* eslint-disable unicorn/filename-case */
 
-import fs, { promises as fsp } from 'fs'
+import { promises as fsp } from 'fs'
 
 import {
   type FileInfoOptions,
@@ -9,16 +9,14 @@ import {
 } from 'prettier'
 
 export default (resolveConfig?: boolean) => async (file: string) => {
-  if (fs.existsSync(file)) {
-    const stat = await fsp.stat(file)
-    /* If file exists but is actually a directory, getFileInfo might end up
-     * trying to open it and read it as a file, so let's not let that happen.
-     * On the other hand, the tests depend on our not failing out with files
-     * that don't exist, so permit nonexistent files to go through (they appear
-     * to be detected by suffix, so never get read).
-     */
-    if (stat.isDirectory()) return false
-  }
+  const stat = await fsp.stat(file).catch(_error => null)
+  /* If file exists but is actually a directory, getFileInfo might end up trying
+   * to open it and read it as a file, so let's not let that happen.  On the
+   * other hand, the tests depend on our not failing out with files that don't
+   * exist, so permit nonexistent files to go through (they appear to be
+   * detected by suffix, so never get read).
+   */
+  if (stat?.isDirectory()) return false
   const config = (await prettierResolveConfig(file, {
     editorconfig: true,
   })) as FileInfoOptions
