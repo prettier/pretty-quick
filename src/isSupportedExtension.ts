f072509bbe5761e64c52c45aeb41176edddb2bc0
Conrad Hughes <90639+ConradHughes@users.noreply.github.com>
2025-03-04 17:56:46 +0000

    fix: don't getFileInfo on symlinks to directories

--- src/isSupportedExtension.ts
+++ src/isSupportedExtension.ts
@@ -1,5 +1,7 @@
 /* eslint-disable unicorn/filename-case */
 
+import fs, { promises as fsp } from 'fs'
+
 import {
   type FileInfoOptions,
   getFileInfo,
@@ -7,6 +9,16 @@ import {
 } from 'prettier'
 
 export default (resolveConfig?: boolean) => async (file: string) => {
+  if (fs.existsSync(file)) {
+    const stat = await fsp.stat(file)
+    /* If file exists but is actually a directory, getFileInfo might end up
+     * trying to open it and read it as a file, so let's not let that happen.
+     * On the other hand, the tests depend on our not failing out with files
+     * that don't exist, so permit nonexistent files to go through (they appear
+     * to be detected by suffix, so never get read).
+     */
+    if (stat.isDirectory()) return false
+  }
   const config = (await prettierResolveConfig(file, {
     editorconfig: true,
   })) as FileInfoOptions
