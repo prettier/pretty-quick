20d90cd6f09db3d64f38aae4c88996c093248fbb
Conrad Hughes <90639+ConradHughes@users.noreply.github.com>
2025-03-04 18:31:35 +0000

    style: code review feedback

--- src/isSupportedExtension.ts
+++ src/isSupportedExtension.ts
@@ -1,6 +1,6 @@
 /* eslint-disable unicorn/filename-case */
 
-import { promises as fsp } from 'fs'
+import fs from 'fs/promises'
 
 import {
   type FileInfoOptions,
@@ -9,14 +9,16 @@ import {
 } from 'prettier'
 
 export default (resolveConfig?: boolean) => async (file: string) => {
-  const stat = await fsp.stat(file).catch(_error => null)
+  const stat = await fs.stat(file).catch(_error => null)
   /* If file exists but is actually a directory, getFileInfo might end up trying
    * to open it and read it as a file, so let's not let that happen.  On the
    * other hand, the tests depend on our not failing out with files that don't
    * exist, so permit nonexistent files to go through (they appear to be
    * detected by suffix, so never get read).
    */
-  if (stat?.isDirectory()) return false
+  if (stat?.isDirectory()) {
+    return false
+  }
   const config = (await prettierResolveConfig(file, {
     editorconfig: true,
   })) as FileInfoOptions
